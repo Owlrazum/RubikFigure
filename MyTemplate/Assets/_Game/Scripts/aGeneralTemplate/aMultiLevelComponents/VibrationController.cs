6229abb910fca613eb9517791e7f7732b2f1b399
Owlrazum <owlrazum@gmail.com>
2022-01-14 11:14:19 +0600

    first iteration on vibration controller

--- MyTemplate/Assets/_Game/Scripts/aGeneralTemplate/aMultiLevelComponents/VibrationController.cs
+++ MyTemplate/Assets/_Game/Scripts/aGeneralTemplate/aMultiLevelComponents/VibrationController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// It was copied from the chainsaw. Needs modifying so it will fit general template.
+/// Subscribe to necessary events with relevant methods.
 /// </summary>
 public class VibrationController : MonoBehaviour
 {
@@ -12,9 +12,9 @@ public class VibrationController : MonoBehaviour
     [Space]
     [SerializeField]
     [Tooltip("In milliseconds")]
-    private int vibrationTime = 200;
+    private int vibrationTime = 50;
 
-    [Header("Custom Vibration")]
+    [Header("Custom Vibrations")]
     [Space]
     [SerializeField]
     private string differentiateFields;
@@ -26,71 +26,35 @@ public class VibrationController : MonoBehaviour
         public int pauseTime;
     }
 
-    [Header("Saw Vibration")]
+    [Header("First type Vibration sequence")]
     [Space]
     [SerializeField]
     [Tooltip("If not needed set it to zero.\n" +
     "Unity developer has no power to remove it...")]
-    private int sawStartDelay = 0;
+    private int firstStartDelay = 0;
 
     [SerializeField]
     [Tooltip("Use it if there is a custom vibration with a pattern.\n" +
         "Vibr, pause, vibr, pause...")]
-    private List<VibrationData> sawVibrationPatternData;
+    private List<VibrationData> firstVibrationPatternData;
 
     [SerializeField]
-    private bool shouldRepeatSawPattern = true;
+    private bool shouldRepeatFirstPattern = true;
 
-    [Header("Fail Vibration")]
+    [Header("Second type Vibration sequence")]
     [Space]
     [SerializeField]
     [Tooltip("If not needed set it to zero.\n" +
     "Unity developer has no power to remove it...")]
-    private int failStartDelay = 0;
+    private int secondStartDelay = 0;
 
     [SerializeField]
     [Tooltip("Use it if there is a custom vibration with a pattern.\n" +
         "Vibr, pause, vibr, pause...")]
-    private List<VibrationData> failVibrationPatternData;
+    private List<VibrationData> secondVibrationPatternData;
 
     [SerializeField]
-    private bool shouldRepeatFailPattern = true;
-
-    [Header("Grind Vibration")]
-    [Space]
-    [SerializeField]
-    [Tooltip("If not needed set it to zero.\n" +
-    "Unity developer has no power to remove it...")]
-    private int grindStartDelay = 0;
-
-    [SerializeField]
-    [Tooltip("Use it if there is a custom vibration with a pattern.\n" +
-        "Vibr, pause, vibr, pause...")]
-    private List<VibrationData> grindVibrationPatternData;
-
-    [SerializeField]
-    private bool shouldRepeatGrindPattern = true;
-
-    [Header("Color Vibration")]
-    [Space]
-    [SerializeField]
-    [Tooltip("If not needed set it to zero.\n" +
-    "Unity developer has no power to remove it...")]
-    private int colorStartDelay = 0;
-
-    [SerializeField]
-    [Tooltip("Use it if there is a custom vibration with a pattern.\n" +
-        "Vibr, pause, vibr, pause...")]
-    private List<VibrationData> colorVibrationPatternData;
-
-    [SerializeField]
-    private bool shouldRepeatColorPattern = true;
-
-    [Header("Stameska")]
-    [Space]
-    [SerializeField]
-    private long stameskaVibrationTime;
-
+    private bool shouldRepeatSecondPattern = true;
 
     private bool isAllowedToVibrate;
     public bool IsAllowedToVibrate
@@ -104,7 +68,7 @@ public class VibrationController : MonoBehaviour
             isAllowedToVibrate = value;
             if (!isAllowedToVibrate)
             {
-                StopCuttingVibration();
+                //StopCuttingVibration();
             }
         }
     }
@@ -118,77 +82,46 @@ public class VibrationController : MonoBehaviour
         Vibration.Vibrate(vibrationTime);
     }
 
-    private long[] sawVibrationPattern;
-    private long[] grindVibrationPattern;
-    private long[] colorVibrationPattern;
-
-    private long[] failVibrationPattern;
+    private long[] firstVibrationPattern;
+    private long[] secondVibrationPattern;
 
     private void Awake()
     {
         Vibration.Init();
 
-        InitializeSawVibrationPattern();
-        InitializeFailVibrationPattern();
-        InitializeGrindVibrationPattern();
-        InitializeColorVibrationPattern();
+        InitializeFirstVibrationPattern();
+        InitializeSecondVibrationPattern();
     }
 
-    private void InitializeSawVibrationPattern()
+    private void InitializeFirstVibrationPattern()
     {
-        sawVibrationPattern = new long[1 + sawVibrationPatternData.Count * 2];
-        sawVibrationPattern[0] = sawStartDelay;
+        firstVibrationPattern = new long[1 + firstVibrationPatternData.Count * 2];
+        firstVibrationPattern[0] = firstStartDelay;
 
         int index = 1;
-        for (int i = 0; i < sawVibrationPatternData.Count; i++)
+        for (int i = 0; i < firstVibrationPatternData.Count; i++)
         {
-            sawVibrationPattern[index++] = sawVibrationPatternData[i].vibrationTime;
-            sawVibrationPattern[index++] = sawVibrationPatternData[i].pauseTime;
+            firstVibrationPattern[index++] = firstVibrationPatternData[i].vibrationTime;
+            firstVibrationPattern[index++] = firstVibrationPatternData[i].pauseTime;
         }
     }
 
-    private void InitializeGrindVibrationPattern()
+    private void InitializeSecondVibrationPattern()
     {
-        grindVibrationPattern = new long[1 + grindVibrationPatternData.Count * 2];
-        grindVibrationPattern[0] = grindStartDelay;
+        secondVibrationPattern = new long[1 + secondVibrationPatternData.Count * 2];
+        secondVibrationPattern[0] = secondStartDelay;
 
         int index = 1;
-        for (int i = 0; i < grindVibrationPatternData.Count; i++)
+        for (int i = 0; i < secondVibrationPatternData.Count; i++)
         {
-            grindVibrationPattern[index++] = grindVibrationPatternData[i].vibrationTime;
-            grindVibrationPattern[index++] = grindVibrationPatternData[i].pauseTime;
-        }
-    }
-
-    private void InitializeColorVibrationPattern()
-    {
-        colorVibrationPattern = new long[1 + colorVibrationPatternData.Count * 2];
-        colorVibrationPattern[0] = colorStartDelay;
-
-        int index = 1;
-        for (int i = 0; i < colorVibrationPatternData.Count; i++)
-        {
-            colorVibrationPattern[index++] = colorVibrationPatternData[i].vibrationTime;
-            colorVibrationPattern[index++] = colorVibrationPatternData[i].pauseTime;
-        }
-    }
-
-    private void InitializeFailVibrationPattern()
-    {
-        failVibrationPattern = new long[1 + failVibrationPatternData.Count * 2];
-        failVibrationPattern[0] = failStartDelay;
-
-        int index = 1;
-        for (int i = 0; i < failVibrationPatternData.Count; i++)
-        {
-            failVibrationPattern[index++] = failVibrationPatternData[i].vibrationTime;
-            failVibrationPattern[index++] = failVibrationPatternData[i].pauseTime;
+            secondVibrationPattern[index++] = secondVibrationPatternData[i].vibrationTime;
+            secondVibrationPattern[index++] = secondVibrationPatternData[i].pauseTime;
         }
     }
 
     private bool isVibrating;
 
-    public void StartCuttingVibration()
+    public void OnFirstVibration()
     {
         if (!IsAllowedToVibrate)
         {
@@ -198,11 +131,11 @@ public class VibrationController : MonoBehaviour
         if (!isVibrating)
         {
             isVibrating = true;
-            Vibration.Vibrate(sawVibrationPattern, shouldRepeatSawPattern ? 0 : -1);
+            Vibration.Vibrate(firstVibrationPattern, shouldRepeatFirstPattern ? 0 : -1);
         }
     }
 
-    public void StopCuttingVibration()
+    public void OnCuttingVibration()
     {
         if (!IsAllowedToVibrate)
         {
@@ -223,15 +156,6 @@ public class VibrationController : MonoBehaviour
         isVibrating = false;
     }
 
-    public void VibrateForStameska()
-    {
-        if (!IsAllowedToVibrate)
-        {
-            return;
-        }
-        Vibration.Vibrate(stameskaVibrationTime);
-    }
-
     public void StartFailVibration()
     {
         if (!IsAllowedToVibrate)
@@ -242,67 +166,11 @@ public class VibrationController : MonoBehaviour
         if (!isVibrating)
         {
             isVibrating = true;
-            Vibration.Vibrate(failVibrationPattern, shouldRepeatFailPattern ? 0 : -1);
+            Vibration.Vibrate(secondVibrationPattern, shouldRepeatSecondPattern ? 0 : -1);
         }
     }
 
     public void StopFailVibration()
-    {
-        if (!IsAllowedToVibrate)
-        {
-            return;
-        }
-
-        if (isVibrating)
-        {
-            StartCoroutine(OnNextFrameFalseIsVibrating());
-            Vibration.Cancel();
-        }
-    }
-
-    public void StartGrindingVibration()
-    {
-        if (!IsAllowedToVibrate)
-        {
-            return;
-        }
-
-        if (!isVibrating)
-        {
-            isVibrating = true;
-            Vibration.Vibrate(grindVibrationPattern, shouldRepeatGrindPattern ? 0 : -1);
-        }
-    }
-
-    public void StopGrindingVibration()
-    {
-        if (!IsAllowedToVibrate)
-        {
-            return;
-        }
-
-        if (isVibrating)
-        {
-            StartCoroutine(OnNextFrameFalseIsVibrating());
-            Vibration.Cancel();
-        }
-    }
-
-    public void StartColorVibration()
-    {
-        if (!IsAllowedToVibrate)
-        {
-            return;
-        }
-
-        if (!isVibrating)
-        {
-            isVibrating = true;
-            Vibration.Vibrate(colorVibrationPattern, shouldRepeatColorPattern ? 0 : -1);
-        }
-    }
-
-    public void StopColorVibration()
     {
         if (!IsAllowedToVibrate)
         {
